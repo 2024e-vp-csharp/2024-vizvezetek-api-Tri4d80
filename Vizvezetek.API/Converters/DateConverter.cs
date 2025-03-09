2fa78279772ab585d813316239d1cec70869c82c
Tri4d80 <bsze3simgab@vasvari.org>
2025-03-09 20:00:32 +0100

    Create Date Converter

---
+++ Vizvezetek.API/Converters/DateConverter.cs
@@ -0,0 +1,13 @@
+﻿// Converters/DateConverter.cs
+using System;
+
+namespace Vizvezetek.API.Converters
+{
+    public static class DateConverter
+    {
+        public static DateTime DateOnlyToDateTime(DateOnly dateOnly)
+        {
+            return new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day);
+        }
+    }
+}
