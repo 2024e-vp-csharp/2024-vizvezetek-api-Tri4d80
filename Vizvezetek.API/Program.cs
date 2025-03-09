731408ecac78753c593122e49caa28781fe9e17e
Tri4d80 <bsze3simgab@vasvari.org>
2025-03-09 21:53:44 +0100

    Update Program.cs & Solution

--- Vizvezetek.API/Program.cs
+++ Vizvezetek.API/Program.cs
@@ -1,25 +1,63 @@
-namespace Vizvezetek.API
+// Program.cs (kiterjesztve a Swagger dokumentációval)
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+using Vizvezetek.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// DB Context konfiguráció
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+builder.Services.AddDbContext<vizvezetekContext>(
+    options => options.UseMySql(connectionString, serverVersion));
+
+// Swagger/OpenAPI konfiguráció
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-    public class Program
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
-        public static void Main(string[] args)
+        Title = "Tiszta Viz Kft. API",
+        Version = "v1",
+        Description = "Web API a vízvezeték-szerelõ munkalapok kezeléséhez.",
+        Contact = new OpenApiContact
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            Name = "Tiszta Viz Kft.",
+            Email = "info@tisztaviz.hu"
         }
-    }
+    });
+
+    // XML dokumentáció hozzáadása
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    options.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tiszta Viz Kft. API v1");
+
+        // Swagger UI az alkalmazás gyökerén
+        c.RoutePrefix = string.Empty; 
+    });
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
